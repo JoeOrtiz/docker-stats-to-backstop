7e5f28d86e10dda42abdc00e5da1e29b29ee2f83
JoeOrtiz <joe.s.ortiz@gmail.com>
2015-05-06 20:48:31 -0700

    Update translate.go

--- translate/translate.go
+++ translate/translate.go
@@ -74,11 +74,11 @@ func cpuCores(stats *docker.ContainerStats) *uint64 {
 func cpuPercent(stats *docker.ContainerStats) *uint64 {
 	var (
 		cpuPercent = 0.0
-	        cpuDelta := float64(stats.CpuStats.CpuUsage.TotalUsage)
-                systemDelta := float64(stats.CpuStats.SystemUsage)
+	        cpuDelta := stats.CpuStats.CpuUsage.TotalUsage
+                systemDelta := stats.CpuStats.SystemUsage
 	)
-	if systemDelta > 0.0 && cpuDelta > 0.0 {
-                containercpuPercent := uint64((cpuDelta / systemDelta) * uint64(len(stats.CpuStats.CpuUsage.PercpuUsage)) * 100.0)
+	if systemDelta != nil && cpuDelta != nil {
+                containercpuPercent := (*cpuDelta / *systemDelta) * uint64(len(stats.CpuStats.CpuUsage.PercpuUsage)) * 100.0
 	}
         return &containercpuPercent
 }
